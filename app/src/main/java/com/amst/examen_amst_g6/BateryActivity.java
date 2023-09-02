bf4cc62d7ac5b107a7762898f2bc46add5b6bca6
Jorge Zambrano <joraazam@espol.edu.ec>
2023-09-01 20:20:49 -0500

    Pantalla principa de presentacion

---
+++ app/src/main/java/com/amst/examen_amst_g6/BateryActivity.java
@@ -0,0 +1,14 @@
+package com.amst.examen_amst_g6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BateryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batery);
+    }
+}
