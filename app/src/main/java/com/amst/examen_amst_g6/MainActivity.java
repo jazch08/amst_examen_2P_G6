e6b6a8285881cc421c83a189ff3cc72565489d86
Jorge Zambrano <joraazam@espol.edu.ec>
2023-09-01 21:41:52 -0500

    Click botones, inicio de servicio de firebase y dependencias de grafico

--- app/src/main/java/com/amst/examen_amst_g6/MainActivity.java
+++ app/src/main/java/com/amst/examen_amst_g6/MainActivity.java
@@ -2,7 +2,9 @@ package com.amst.examen_amst_g6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goBatery(View view){
+        Intent intent = new Intent(this, BateryActivity.class);
+        startActivity(intent);
+    }
+
+    public void goData(View view){
+        Intent intent = new Intent(this, DataActivity.class);
+        startActivity(intent);
+    }
+
+
 }
